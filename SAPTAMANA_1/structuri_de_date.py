c0ddc73d0430b2a791292df432fffb641753c75b
unknown <ardeleansamuel@gmail.com>
2022-06-29 21:31:54 +0300

    liste

---
+++ SAPTAMANA_1/structuri_de_date.py
@@ -0,0 +1,25 @@
+# structuri de date
+my_list = [8, 2, 'a', 8, '4']
+#print('mesaj')
+#print(my_list[3])
+#print(my_list[-1])
+my_list.append('ana')
+#print(len(my_list)) - lungimea listei
+#my_list.append('ana') - adauga valori la sfarsitul listei
+#print(my_list)
+#print(my_list.index('a')) - ne arata pozitia nuumarului/ caracterului in lista, doar prima pozitie a indexului gasita
+
+#print(my_list.index(8))
+#my_list.insert(4, 'A')
+#print(my_list)
+#my_list.insert(7,'A') - daca ai la index un nr mai mare decat cat elista de lunga, va pune caracterul la sfarsitul listei
+
+#print(my_list[2:5]) - afiseaza un calup de la o pozitie la cealalta pozitie
+
+#print(my_list[2:1])
+#print(my_list[2:3:2])
+
+my_list.append(['a', 'b', 3,'x',[4,7,'w']])
+print(my_list)
+print(my_list[5])
+print(my_list[6][4][2])
