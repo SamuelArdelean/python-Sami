6eb7169b6b1177da1e64a8eea6d70526ef6b5557
unknown <ardeleansamuel@gmail.com>
2022-06-29 20:22:09 +0300

    primul nostru commit

---
+++ SAPTAMANA_1/variabile.py
@@ -0,0 +1,59 @@
+"""Dezvoltarea unui program care sa calculeze venitul saptamanal al unui angajat"""
+"""venitul saptamanal= venitul/ ora * nr ore"""
+"""venit_saptamanal= 35 lei* 40 ore"""
+
+#float * float -> float
+#float * integer -> float
+#integer * integer -> integer
+
+#venit_saptamanal ----> float
+#venit_pe_ora ----> float
+#nr_ore ----> float
+
+"""1. obtinem venitul ora"""
+"""2. obtinem numarul de ore lucrate"""
+"""3. calcul venit saptamanal"""
+"""4. afisare rezultate"""
+
+
+#1. Numere- float, integer, nr complexe
+#2. String
+#3. Liste = [2, 3, 5]
+#4. Seturi
+#5. Dictionare
+#6. Tupluri
+#7. Boolean
+
+""" operatori de asignare """
+
+#x = 1
+#y = 3
+#x = x + y
+#print(x)
+
+#x= 1
+#y = 3
+#x += y
+#print(x)
+
+#+=/ -=/ /= / *=
+
+# weekly_income= 10
+# weeklyIncome = 10
+# ex: and, del, from,not, while, as, return, def
+
+#first_number = 1
+#first_Number = first_number + 1
+#print(first_Number)
+
+#example = True
+#if example:
+#    b = 10
+ #   if asda= False:
+  #      example = False
+   # else:
+    #    pass
+
+
+
+
