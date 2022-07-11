36966e01c8d0b4a710fb43a37c06bce707e5c5a3
unknown <ardeleansamuel@gmail.com>
2022-07-11 21:37:15 +0300

    tema 1

---
+++ SAPTAMANA_1/Tema 1.py
@@ -0,0 +1,14 @@
+print('lista initiala este formata din: ')
+lista_initiala = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print(lista_initiala)
+lista_ascendenta = lista_initialaGIT
+lista_ascendenta.sort()
+print('lista ordonata ascendent = ', lista_ascendenta)
+lista_descendenta = lista_initiala
+lista_descendenta.reverse()
+print(' lista ordonata descendent = ', lista_descendenta)
+lista_ascendenta = lista_initiala
+lista_ascendenta.sort()
+print('lista cu valorile pare este = ', lista_ascendenta[1:10:2])
+print('lista cu valorile impare este = ', lista_ascendenta[0:10:2])
+print('lista cu multiplii de 3 este = ', lista_ascendenta[2:10:3])
