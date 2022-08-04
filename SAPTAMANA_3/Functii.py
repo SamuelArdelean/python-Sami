aac2fa27003789191901b05df2ee719237d526b0
unknown <ardeleansamuel@gmail.com>
2022-08-04 19:20:56 +0300

    validator_cnp

--- SAPTAMANA_3/Functii.py
+++ SAPTAMANA_3/Functii.py
@@ -4,8 +4,8 @@
 #var1= "ana are \'3\' mere" #escape, ne aguta sa tratam ghilimeaua ca un string
 #print(var1)
 
-numar_mere = 3
-numar_pere = 2
+#numar_mere = 3
+#umar_pere = 2
 #ar1 = f"ana are {numar_mere} mere si {numar_pere} pere"
 #var1= "ana are {0} mere si {1} pere". format(numar_mere, numar_pere) #pt fiecare pereche de acolada, adaugi o variabila la format
 #var1= "ana are " + str(numar_mere) + " mere si " + str(numar_pere) + " pere"
@@ -23,13 +23,13 @@ numar_pere = 2
 #print(var1)
 
 
-def my_function(a, b:int=0, c:int=0, *args) -> (int, dict):
-    return a + b + c, { 'diferenta' : a - b - c}
+#def my_function(a, b:int=0, c:int=0, *args) -> (int, dict):
+#    return a + b + c, { 'diferenta' : a - b - c}
 
 #suma, diferenta = my_function(numar_mere, c= numar_pere, b=2)
 #print(suma, diferenta)
 
-def suma(a, b, *args, **kwargs):
+#def suma(a, b, *args, **kwargs):
     '''
     :param a: primul parametru
     :param b: al doilea parametru
@@ -37,37 +37,41 @@ def suma(a, b, *args, **kwargs):
     :param kwargs: argumente tip cheie - valoare
     :return: suma tuturor parametrilor
     '''
-    total = 0
-    variabila_temp = a + b
-    for i in args:
-        total = total + i    #definita doar in functia suma, nu este disponibila in afara, DECAT daca returnam
+#    total = 0
+#    variabila_temp = a + b
+#    for i in args:
+#        total = total + i    #definita doar in functia suma, nu este disponibila in afara, DECAT daca returnam
 #    print(type(args))
-    print(type(kwargs)) #dictionar
-    for i, v in kwargs.items():
-        print(i, v)
-        total = total + v
-    return variabila_temp + total
-    print(' ana are mere')  # asta nu se executa, ca este dupa return
+#    print(type(kwargs)) #dictionar
+#    for i, v in kwargs.items():
+#        print(i, v)
+#        total = total + v
+#    return variabila_temp + total
+#    print(' ana are mere')  # asta nu se executa, ca este dupa return
 
-variabila = suma(1, 2, 3, 4, 5, c=4, d=7, e=9)    #functia args ne permite sa adaugam oricati parametrii dorim, dar acesti parametrii ar trebuii parcursi
-print(variabila)
+#variabila = suma(1, 2, 3, 4, 5, c=4, d=7, e=9)    #functia args ne permite sa adaugam oricati parametrii dorim, dar acesti parametrii ar trebuii parcursi
+#print(variabila)
 
 
 #EXCEPTII
-my_var = input('adauga un numar: ')
-try:
-    my_int = int(my_var)   #teoretic nu se poate converti string in int
-    print(my_int)
-    variabila_nedefinita = None
-    print(variabila_nedefinita)
-except NameError:
-    print('variabila nu este definita')
-    variabila_nedefinita = 0
-except ValueError:
-    print(' eroare de valoare')
-except Exception as e:
-    print('exceptie', e)
-else:   # apare doar cand nu sunt exceptii
-    print(' nu sunt exceptii')
-finally:
-    print(' s-a rulat programul')
+#my_var = input('adauga un numar: ')
+#try:
+#    my_int = int(my_var)   #teoretic nu se poate converti string in int
+#    print(my_int)
+#    variabila_nedefinita = None
+#    print(variabila_nedefinita)
+#except NameError:
+#    print('variabila nu este definita')
+#    variabila_nedefinita = 0
+#except ValueError:
+#    print(' eroare de valoare')
+#except Exception as e:
+#    print('exceptie', e)
+#else:   # apare doar cand nu sunt exceptii
+#    print(' nu sunt exceptii')
+#finally:
+#    print(' s-a rulat programul')
+
+def test_function(first, second, third):
+    result = first*3 + second*3 + third*3
+    return result
--- SAPTAMANA_3/validator_cnp.py
+++ SAPTAMANA_3/validator_cnp.py
@@ -10,6 +10,16 @@ def validare(variabila):
         variabila = input("reintrodu CNP-ul: ")
     return variabila
 
+def validare_judet(cnp):
+    judet = int(cnp[7:9])
+    if judet < 47:
+        return True
+    elif judet == (51 or 52):
+        return True
+    elif judet != (47 and 48 and 49 and 50):
+        return False
+    else:
+        return False
 
 def validare_sex(a):
     if int(a[0]) in range(1, 10):
@@ -22,17 +32,16 @@ def validare_data_nastere(cnp):
     try:
         datetime.datetime.strptime(data_nastere, '%y%m%d')  #primul datetime este pachetul de date calendaristice
         return True
-    except Exception:
+    except Exception:  #clasa generala care contine toate exceptiile standard din python, modalitate de a trata erorile din cod
         return False
 
 
 def validator_cnp(cnp):
     """
-
     :param cnp: cnp-ul introdus de utilizator de la tastatura
     :return: mesaj "valid" in cazul in care cnp-ul este valid; "invalid" in cazul in care cnp-ul nu e valid
     """
-    if cnp and validare_sex(cnp) and validare_data_nastere(cnp):
+    if cnp and validare_sex(cnp) and validare_data_nastere(cnp) and validare_judet(cnp):
         return "valid"
     return "invalid"
 
